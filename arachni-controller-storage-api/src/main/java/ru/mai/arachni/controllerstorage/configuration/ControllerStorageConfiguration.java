9f75d203295fab85eef56475d725daae96b764e3
docNemo <varyag300300@yandex.ru>
2023-10-17 02:00:24 +0300

    devslaves-971395 : fix sec

--- arachni-controller-storage-api/src/main/java/ru/mai/arachni/controllerstorage/configuration/ControllerStorageConfiguration.java
+++ arachni-controller-storage-api/src/main/java/ru/mai/arachni/controllerstorage/configuration/ControllerStorageConfiguration.java
@@ -1,7 +1,6 @@
 package ru.mai.arachni.controllerstorage.configuration;
 
 import io.minio.MinioClient;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
