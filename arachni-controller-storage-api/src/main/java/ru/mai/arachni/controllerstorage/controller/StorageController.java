bd1267b7dc4ff7ba96f36a52154407f8df1f7fc9
docNemo <varyag300300@yandex.ru>
2023-10-19 01:47:22 +0300

    devslaves-971395 : redesign api

--- arachni-controller-storage-api/src/main/java/ru/mai/arachni/controllerstorage/controller/StorageController.java
+++ arachni-controller-storage-api/src/main/java/ru/mai/arachni/controllerstorage/controller/StorageController.java
@@ -3,6 +3,7 @@ package ru.mai.arachni.controllerstorage.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,8 +28,8 @@ public class StorageController {
         );
     }
 
-    @GetMapping("/download")
-    public String downloadObject(@RequestParam String objectName) {
+    @GetMapping("/download/{objectName}")
+    public String downloadObject(@PathVariable String objectName) {
         return minioStorageService.downloadObject(objectName);
     }
 
